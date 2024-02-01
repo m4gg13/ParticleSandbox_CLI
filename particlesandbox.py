657f3583144f82af86a7ad229bb7a62ce7aa949c
Maggie Zirnhelt <maggie.zirnhelt@jamf.com>
2024-01-31 21:08:47 -0500

    lookin pretty good, pretty clean, maybe ready to work on simulation

--- particlesandbox.py
+++ particlesandbox.py
@@ -9,6 +9,7 @@ from simulation import run_simulation
 
 def modus_operandi():
     print("|-----Modus Operandi-----|\n")
+    global modus_operandi
 
     while True:
         with open("config.json") as config:
@@ -20,14 +21,14 @@ def modus_operandi():
             # if they answered "neither" they need to try again
             break
 
-        modus_operandi = config.read()
-
+        with open("config.json") as config:
+            modus_operandi = config.read()
     print("\n|------------------------|\n\n")
-
     return modus_operandi
 
 def initial_state():
     print("|-------Initial State-------|\n")
+    global initial_state
 
     while True:
         with open("initial_state.json") as initial_state:
@@ -44,44 +45,40 @@ def initial_state():
                 break
             else:
                 print(f'Invalid state: {error}')
-
     print("\n|------------------------|\n\n")
-
     return initial_state
 
 def steps():
     print("|----------Steps----------|\n")
+    global steps
 
     while True:
-        steps = input("How many steps in time would you like to take?\n")
+        steps = input("How many steps in time would you like to take?\n>")
 
         error = validate_steps(steps)
         if not error:
             break
         else:
             print(f'Invalid number or steps: {error}')
-
     print("\n|------------------------|\n\n")
-
     return steps
 
 def direction():
     print("|--------Direction--------|\n")
+    global forward
 
     while True:
-        direction = input("Would you like to move forward or backward in time?\n")
+        direction = input("Would you like to move forward or backward in time?\n>")
 
         d = direction.lower()
-        if d == "forward" or d == ">":
+        if d == "forward" or d == "f" or d == ">":
             forward = True
-            return
-        elif d == "backward" or d == "<":
+        elif d == "backward" or d == "b" or d == "<":
             forward = False
-            return
         else:
             print("Please say 'Forward' or 'Backward'")
-
     print("\n|------------------------|\n\n")
+    return forward
 
 def validate_state_syntax(state):
 	# this feels dangerous but i can't tell why...
@@ -117,10 +114,10 @@ def wizard():
     m = modus_operandi()
     i = initial_state()
     s = steps()
-    d = direction()
+    f = direction()
 
     print("*+>~.. *+>~.......~<+* ..~<+*\n\n")
 
-    run_simulation(modus_operandi, initial_state, steps, forward)
+    run_simulation(m, i, s, f)
 
 wizard()

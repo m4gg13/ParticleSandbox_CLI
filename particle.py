07183ce90e786514601ab8f88321cfb683f9977f
Maggie Zirnhelt <maggie.zirnhelt@jamf.com>
2024-02-10 12:12:07 -0500

    added some more particles and details

---
+++ particle.py
@@ -0,0 +1,64 @@
+class Particle:
+    def __init__(self, number, mass, coordinate):
+        self.number = number
+        self.mass = 2
+        self.coordinate = 3
+        self.name = "generic"
+
+class Fermion:
+    def __init__(self, number):
+        self.fundamental_class = ["fermion"]
+        self.interactions = ["strong", "weak", "electromagnetic", "gravity"]
+
+class UpQuark(Fermion):
+    def __init__(self, number):
+        self.number = number
+        self.name = "up"
+        self.symbol = "u"
+        self.spin = 0.5
+        self.charge = 2/3
+        self.mass = 2.2 * 10**6
+        self.generation = 1
+        self.family = "quark"
+        self.decay_product = {
+            "stable":0,
+            "down":1,
+            "positron":1,
+            "electron neutrino":1
+        }
+
+class DownQuark(Fermion):
+    def __init__(self, number):
+        self.number = number
+        self.name = "down"
+        self.symbol = "d"
+        self.spin = 0.5
+        self.charge = -1/3
+        self.mass = 4.7 * 10**6
+        self.generation = 1
+        self.family = "quark"
+        self.decay_product = {
+            "stable":0,
+            "up":1,
+            "electron":1,
+            "electron antineutrino":1
+         }
+
+class Proton:
+    def __init__(self, number):
+        self.number = number
+        self.name = "proton"
+        self.spin = 0.5
+        self.charge = 1
+        self.mass = 938.272 * 10**6
+
+
+# notes
+# mass is measured in electron volts per speed of light squared
+# so a mass * 10**6 means Mega electron volts per speed of light squared
+# `fundamental_class` - can be `fermion`, 'boson', and `hadron`
+#
+# according to https://en.wikipedia.org/wiki/Up_quark
+# up quarks decay into stable or down quark + positron + electron neutrino
+#
+#

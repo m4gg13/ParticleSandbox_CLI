f3131a36bcbd9aa0ded8824386db43b0fa7cac46
m4gg13 <maggie.zirnhelt@gmail.com>
2024-03-31 19:58:02 -0400

    other things that go with the previous change

--- atomproblem.py
+++ atomproblem.py
@@ -7,14 +7,13 @@ from qiskit_nature.second_q.mappers import JordanWignerMapper
 
 import json
 
-# TODO
-# 1. make a param that controls whether comparison is printed
-# 2. find a way to determine and report what the final state
+# TODO: find a way to determine and report what the final state
 #   of the system is in terms of type like `matter`, `particle`, `atom`, `molecule`
 #   since for ex H_2 atomic input state ends up being a molecule output state
 def evolve(state, basis:str="sto3g", charge:int=0, spin:int=0, print_all:bool=False, print_comparison:bool=False):
     # make atom - looks like this `"H 0 0 0; H 0 0 0.735"`
     atoms = translate_state_to_atoms(state)
+    final_stateobj = AtomProblemState("initial", state, atoms, basis, charge, spin, print_all, print_comparison)
     # determine the problem
     driver = PySCFDriver(
         atom=atoms,
@@ -57,7 +56,44 @@ def evolve(state, basis:str="sto3g", charge:int=0, spin:int=0, print_all:bool=Fa
     if print_comparison:
         print_problem_result(problem, result, max_problem_energy, initial_energy, final_energy, spin)
     final_state = translate_atoms_to_state(final_atoms)
-    return final_state
+    # TODO: the charge and spin are not accurate here pretty sure
+    initial_state = state
+    initial_atoms = []
+    initial_spin = spin
+    final_spin = result.spin
+    initial_num_particles = atoms.count(';') + 1
+    final_num_particles = result.num_particles
+    result = EvolutionSummary(initial_state,
+                              final_state,
+                              initial_atoms,
+                              final_atoms,
+                              initial_energy,
+                              final_energy,
+                              initial_spin,
+                              final_spin,
+                              initial_num_particles,
+                              final_num_particles,
+                              hamiltonian,
+                              problem)
+    return result
+
+def get_json_evolution_result(state,
+                              basis:str="sto3g",
+                              charge:int=0, spin:int=0,
+                              print_all:bool=False,
+                              print_comparison:bool=False):
+    result = evolve(state, basis, charge, spin, print_all, print_comparison)
+    final_state = result.final_state
+    final_atoms = result.final_atoms
+    final_stateobj = AtomProblemState("final",
+                                      final_state,
+                                      final_atoms,
+                                      basis,
+                                      charge,
+                                      spin,
+                                      print_all,
+                                      print_comparison)
+    return "{\"hydrogen\": 2}"
 
 def determine_candidate_states():
     # 1. gotta figure out which results have the right number of atoms
@@ -95,6 +131,18 @@ def translate_atoms_to_state(atoms):
             final_state_dictionary[key] = 1
     final_state = json.dumps(final_state_dictionary)
     return final_state
+
+# (state, basis, charge, spin, print_all, print_comparison)
+class AtomProblemState:
+    def __init__(self, stage, state, atoms, basis, charge, spin, print_all, print_comparison):
+        self.stage = stage
+        self.state = state
+        self.atoms = atoms
+        self.basis = basis
+        self.charge = charge
+        self.spin = spin
+        self.print_all = print_all
+        self.print_comparison = print_comparison
 
 # MARK: logging
 
@@ -137,6 +185,17 @@ def print_hamiltonian_details(hamiltonian):
     second_q_op = hamiltonian.second_q_op()
     print(second_q_op)
     print("----------")
+
+def hamiltonian_to_string(hamiltonian):
+    str = "-----HAMILTONIAN-----\n"
+    str = str + "hamiltonian.electronic_integrals.alpha:\n"
+#    coefficients = hamiltonian.electronic_integrals
+#    str = str + coefficients
+#    str = str + "\n"
+    str = str + hamiltonian.second_q_op()
+    str = str + "\n"
+    str = str + "----------\n"
+    return str
 
 def print_problem_details(problem):
     print("-----PROBLEM-----")
@@ -272,6 +331,19 @@ def print_energy_details(initial_energy, final_energy):
     print(final_energy)
     print("----------")
 
+def evolution_to_string(evolution_state):
+    evolution_str = ""
+#    str = hamiltonian_to_string(evolution_state.hamiltonian)
+#    str = str + "\n"
+    evolution_str = evolution_str + evolution_state.problem
+    evolution_str = evolution_str + "\n"
+    evolution_str = evolution_str + evolution_state.result
+    evolution_str = evolution_str + "\n"
+    evolution_str = evolution_str + evolution_state.initial_energy
+    evolution_str = evolution_str + "\n"
+    evolution_str = evolution_str + evolution_state.final_energy
+    return evolution_str
+
 def print_all(hamiltonian, problem, result, initial_energy, final_energy):
     print_hamiltonian_details(hamiltonian)
     print_problem_details(problem)
@@ -294,3 +366,31 @@ def print_problem_result(problem, result, max_problem_energy, initial_energy, fi
 #    print("| orbital_energies_b | spin | " + str(problem.orbital_energies_b) + " | " + str(result.spin) + " |")
     print("| initial spin | spin | " + str(initial_spin) + " | " + str(result.spin) + " |")
     print("| initial energy | final energy | " + str(initial_energy) + " | " + str(final_energy) + " |")
+
+class EvolutionSummary:
+    def __init__(self,
+                 initial_state,
+                 final_state,
+                 initial_atoms,
+                 final_atoms,
+                 initial_energy,
+                 final_energy,
+                 initial_spin,
+                 final_spin,
+                 initial_num_particles,
+                 final_num_particles,
+                 hamiltonian,
+                 problem):
+        self.initial_state = initial_state
+        self.final_state = final_state
+        self.initial_atoms = initial_atoms
+        self.final_atoms = final_atoms
+        self.initial_energy = initial_energy
+        self.final_energy = final_energy
+        self.initial_spin = initial_spin
+        self.final_spin = final_spin
+        self.initial_num_particles = initial_num_particles
+        self.final_num_particles = final_num_particles
+        self.hamiltonian = hamiltonian
+        self.problem = problem
+
